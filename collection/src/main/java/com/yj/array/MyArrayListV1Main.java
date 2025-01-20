cc10d593772f0a5abbd3dfbd0c30791f0217f9b6
agida0413 <144654789+agida0413@users.noreply.github.com>
2025-01-20 20:06:36 +0900

    ArrayList

---
+++ collection/src/main/java/com/yj/array/MyArrayListV1Main.java
@@ -0,0 +1,10 @@
+package com.yj.array;
+
+public class MyArrayListV1Main {
+    public static void main(String[] args) {
+        MyArrayListV1 list = new MyArrayListV1();
+        System.out.println("데이터추가");
+
+        System.out.println(list);
+    }
+}

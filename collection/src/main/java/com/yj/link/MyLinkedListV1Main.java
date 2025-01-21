40501df1357d514f1ac2dd6e84af80735053853d
agida0413 <144654789+agida0413@users.noreply.github.com>
2025-01-21 20:50:39 +0900

    linked list

---
+++ collection/src/main/java/com/yj/link/MyLinkedListV1Main.java
@@ -0,0 +1,7 @@
+package com.yj.link;
+
+public class MyLinkedListV1Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ collection/src/main/java/com/yj/link/NodeMain3.java
@@ -0,0 +1,23 @@
+package com.yj.link;
+
+public class NodeMain3 {
+    public static void main(String[] args) {
+        Node first = new Node("A");
+        first.next = new Node("B");
+        first.next.next = new Node("C");
+
+        System.out.println( first);
+
+
+        //모든 노드 탐색
+        printAll(first);
+
+        //마지막노드
+
+
+    }
+
+    private static void printAll(Node first) {
+
+    }
+}
